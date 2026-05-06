7457088404784a35326782b86e977d62432c9d44
Liya Mary <liyamarybiju@gmail.com>
2026-05-07 00:20:16 +0530

    Fix image loading for production

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from admins.views import home
 
@@ -10,4 +12,4 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('staff/', include('staff.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
